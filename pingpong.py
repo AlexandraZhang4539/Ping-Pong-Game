3056ce21a5031374628e09ae9d5fad8fc6717ba6
AlexandraZhang4539 <zhana8592@wrdsb.ca>
2023-07-13 18:46:37 -0400

    Made Pong Class (Ball)

--- pingpong.py
+++ pingpong.py
@@ -10,22 +10,46 @@ run = True
 FPS = 30
 
 class Player():
-    def __init__(self, player_x, player_y, color, w, h, speed):
+    def __init__(self, player_x, player_y, color, w, h, inputup, inputdown, speed):
         self.rect = Rect (player_x,player_y,w,h)
         self.speed = speed
         self.color = color
+        self.up = inputup
+        self.down = inputdown
 
     def reset (self):
         draw.rect(window, self.color, self.rect)
+    def move (self):
+        keys = key.get_pressed()
+        if keys[self.up]:
+            self.rect.y -= self.speed
+        if keys[self.down]:
+            self.rect.y += self.speed
 
-player1 = Player(30,200, (255,255,255), 10, 100, 5)
-player2 = Player(670,200, (255,255,255), 10, 100, 5)
+class Pong():
+    def __init__(self, ball_x, ball_y, color, radius, speed):
+        self.x = ball_x
+        self.y = ball_y
+        self.radius = radius
+        self.color = color
+        self.speed = speed
+    def reset (self):
+        draw.circle(window, self.color, (self.x, self.y), self.radius)
+
+player1 = Player(670,200, (255,255,255), 10, 100, K_UP, K_DOWN, 15)
+player2 = Player(30,200, (255,255,255), 10, 100, K_w, K_s, 15)
+
+pingpong = Pong (350,250,(255,10,2), 20, 10)
 
 while run: 
     for e in event.get(): #event.get gives a list of all event (look at documentation) so it searches through them for pygame.QUIT
         if e.type == QUIT: #if you click "x" then it exits
             run = False #updating window everytime this runs
+    window.fill(background)
+    pingpong.reset()
     player1.reset()
     player2.reset()
+    player1.move()
+    player2.move()
     display.update()
     clock.tick(FPS)
